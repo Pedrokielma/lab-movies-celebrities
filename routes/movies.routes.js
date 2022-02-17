573a98a00b3b227ba23605eca9c2c4fa86ca7ce2
PedroKielma <peterkielma@gmail.com>
2022-02-17 09:45:36 +0000

    listing movies

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -2,6 +2,32 @@
 const router = require("express").Router();
 
 
+const Movie = require("../models/Movie.model");
+
+router.get("/movies/create", (req, res, next) => {
+    res.render("movies/new-movie")
+})
+
+router.post("/movies/create", (req, res, next) => {
+    const { title, gener, plot, cast } = req.body;
+    Movie
+    .create({ title, gener, plot, cast })
+    .then(() => res.redirect('/movies'))
+    .catch(err => next(err));
+});
+
+
+router.get("/movies", (req, res, next) => {
+    Movie
+    .find()
+    .then((allMovies) => {
+      console.log(allMovies);
+      res.render('movies/movies.hbs', { movies: allMovies });
+    })
+    .catch((err) => {
+      next(err);
+    });
+})
 
 // all your routes here
 
